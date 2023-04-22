ff7c454c40bf181af9238d201577e373d9e41ba1
sari4983 <s1849833@gmail.com>
2023-04-23 00:22:55 +0300

    first commit

---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApi;
+
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool? IsComplete { get; set; }
+
+    public int? IdUser { get; set; }
+
+    public virtual User? IdUserNavigation { get; set; }
+}
